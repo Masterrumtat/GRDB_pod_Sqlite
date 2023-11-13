533122c7a8f13998b088256d480b4e3f9cdef59c
Masterrumtat <captain019377@gmail.com>
2023-11-13 13:44:43 +0700

    Initial Commit

---
+++ GRDB_pod_SqliteUITests/GRDB_pod_SqliteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GRDB_pod_SqliteUITestsLaunchTests.swift
+//  GRDB_pod_SqliteUITests
+//
+//  Created by Vituruch Sinthusate on 13/11/2566 BE.
+//
+
+import XCTest
+
+final class GRDB_pod_SqliteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
